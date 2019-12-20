27d7415f3f3fdc19eace89d91f1555771bf56479
Arthur Carlsson <arthur@kiron.net>
2019-12-20 22:10:20 +0100

    Day 5 part 1

---
+++ rust/day05/src/main.rs
@@ -0,0 +1,425 @@
+use std::io::prelude::*;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let f = std::fs::File::open(std::env::args().nth(1).expect("Could not get arg 1"))
+        .expect("Could not open input file");
+
+    let code: Vec<i32> = read_intcode(f).collect();
+    let mut program = Program::new(code);
+
+    while !program.finished {
+        program.tick();
+    }
+}
+
+fn read_intcode<R>(read: R) -> impl Iterator<Item = i32>
+where
+    R: Read,
+{
+    BufReader::new(read)
+        .split(b',')
+        .map(|l| String::from_utf8(l.unwrap()).unwrap())
+        .flat_map(|s| s.trim().parse::<i32>())
+}
+
+type Pc = usize;
+type Code = Vec<i32>;
+
+#[derive(PartialEq, Debug)]
+struct Program {
+    pc: Pc,
+    code: Code,
+    finished: bool,
+}
+impl Program {
+    fn new(code: Code) -> Self {
+        Self {
+            code,
+            pc: 0,
+            finished: false,
+        }
+    }
+
+    fn tick(&mut self) {
+        let instr = Instr::decode(&self.code[self.pc..]).expect("Could not decode instruction");
+        self.execute_instr(instr, &mut std::io::stdin().lock(), &mut std::io::stdout());
+    }
+
+    fn execute_instr(&mut self, instr: Instr, r#in: &mut dyn BufRead, out: &mut dyn Write) {
+        use Opcode::*;
+
+        macro_rules! param_v {
+            ($params:expr, $pi:expr, $i:expr) => {
+                match $pi {
+                    ParameterMode::Immediate => $params[$i],
+                    ParameterMode::Position => self.code[$params[$i] as usize],
+                }
+            };
+        }
+
+        match instr {
+            Instr(Add(pa, pb), params) => {
+                let (a, b, t) = (param_v!(params, pa, 0), param_v!(params, pb, 1), params[2]);
+
+                self.code[t as usize] = a + b;
+                self.pc += 4;
+            }
+            Instr(Mul(pa, pb), params) => {
+                let (a, b, t) = (param_v!(params, pa, 0), param_v!(params, pb, 1), params[2]);
+
+                self.code[t as usize] = a * b;
+                self.pc += 4;
+            }
+            Instr(In, params) => {
+                let t = params[0];
+
+                out.write_fmt(format_args!("Input: ")).unwrap();
+                out.flush().unwrap();
+
+                let mut line = String::new();
+                r#in.read_line(&mut line).unwrap();
+
+                self.code[t as usize] = line.trim().parse().unwrap();
+                self.pc += 2;
+            }
+            Instr(Out(pa), params) => {
+                let a = param_v!(params, pa, 0);
+
+                out.write_fmt(format_args!("{}", a)).unwrap();
+                self.pc += 2;
+            }
+            Instr(Fin, ..) => self.finished = true,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Opcode {
+    Add(ParameterMode, ParameterMode),
+    Mul(ParameterMode, ParameterMode),
+    In,
+    Out(ParameterMode),
+    Fin,
+}
+impl Opcode {
+    fn decode(i: i32) -> Option<Opcode> {
+        macro_rules! param {
+            ($idx:expr) => {{
+                let ii = i / (100 * 10i32.pow($idx));
+                ParameterMode::parse(ii - 10 * (ii / 10)).unwrap()
+            }};
+        }
+
+        // Pick out the last two digits to determine the opcode
+        match i - 100 * (i / 100) {
+            1 => Some(Opcode::Add(param!(0), param!(1))),
+            2 => Some(Opcode::Mul(param!(0), param!(1))),
+            3 => Some(Opcode::In),
+            4 => Some(Opcode::Out(param!(0))),
+            99 => Some(Opcode::Fin),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Instr(Opcode, Vec<i32>);
+impl Instr {
+    fn decode(code: &[i32]) -> Option<Instr> {
+        let opcode = Opcode::decode(code[0])?;
+
+        let params = match opcode {
+            Opcode::Add(..) => Some(vec![code[1], code[2], code[3]]),
+            Opcode::Mul(..) => Some(vec![code[1], code[2], code[3]]),
+            Opcode::In => Some(vec![code[1]]),
+            Opcode::Out(..) => Some(vec![code[1]]),
+            Opcode::Fin => Some(vec![]),
+        };
+
+        params.map(|p| Instr(opcode, p))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum ParameterMode {
+    Position,
+    Immediate,
+}
+impl ParameterMode {
+    fn parse(n: i32) -> Option<ParameterMode> {
+        match n {
+            0 => Some(ParameterMode::Position),
+            1 => Some(ParameterMode::Immediate),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Parameter(ParameterMode, i32);
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_execute_instr_add() {
+        let mut program = Program::new(vec![0, 1, 2, 0]);
+
+        program.execute_instr(
+            Instr(
+                Opcode::Add(ParameterMode::Position, ParameterMode::Position),
+                vec![1, 2, 0],
+            ),
+            &mut Cursor::new(""),
+            &mut Cursor::new(Vec::new()),
+        );
+
+        assert_eq!(
+            Program {
+                code: vec![3, 1, 2, 0],
+                pc: 4,
+                finished: false
+            },
+            program
+        );
+    }
+
+    #[test]
+    fn test_execute_instr_mul() {
+        let mut program = Program::new(vec![0, 4, 5, 0, 5, 6]);
+
+        program.execute_instr(
+            Instr(
+                Opcode::Mul(ParameterMode::Position, ParameterMode::Position),
+                vec![4, 5, 0],
+            ),
+            &mut Cursor::new(""),
+            &mut Cursor::new(Vec::new()),
+        );
+
+        assert_eq!(
+            Program {
+                pc: 4,
+                code: vec![30, 4, 5, 0, 5, 6],
+                finished: false
+            },
+            program
+        );
+    }
+
+    #[test]
+    fn test_execute_instr_mul_neg() {
+        let mut program = Program::new(vec![0, -4, 5]);
+
+        program.execute_instr(
+            Instr(
+                Opcode::Mul(ParameterMode::Position, ParameterMode::Position),
+                vec![1, 2, 0],
+            ),
+            &mut Cursor::new(""),
+            &mut Cursor::new(Vec::new()),
+        );
+
+        assert_eq!(
+            Program {
+                pc: 4,
+                code: vec![-20, -4, 5],
+                finished: false
+            },
+            program
+        );
+    }
+
+    #[test]
+    fn test_execute_instr_fin() {
+        let mut program = Program::new(vec![99]);
+
+        program.execute_instr(
+            Instr(Opcode::Fin, vec![]),
+            &mut Cursor::new(""),
+            &mut Cursor::new(Vec::new()),
+        );
+
+        assert_eq!(
+            Program {
+                code: vec![99],
+                pc: 0,
+                finished: true
+            },
+            program
+        );
+    }
+
+    #[test]
+    fn test_execute_instr_in() {
+        let mut program = Program::new(vec![3, 1]);
+
+        program.execute_instr(
+            Instr(Opcode::In, vec![1]),
+            &mut Cursor::new("89"),
+            &mut Cursor::new(Vec::new()),
+        );
+
+        assert_eq!(
+            Program {
+                code: vec![3, 89],
+                pc: 2,
+                finished: false
+            },
+            program
+        );
+    }
+
+    #[test]
+    fn test_execute_instr_out() {
+        let mut program = Program::new(vec![4, 10]);
+        let mut buf = Cursor::new(Vec::new());
+
+        program.execute_instr(
+            Instr(Opcode::Out(ParameterMode::Position), vec![1]),
+            &mut Cursor::new(""),
+            &mut buf,
+        );
+
+        assert_eq!(
+            Program {
+                code: vec![4, 10],
+                pc: 2,
+                finished: false
+            },
+            program
+        );
+        assert_eq!("10", std::str::from_utf8(buf.get_ref()).unwrap());
+    }
+
+    #[test]
+    fn test_code() {
+        let expected = vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
+        let mut program = Program::new(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]);
+
+        program.tick();
+        program.tick();
+        program.tick();
+
+        assert_eq!(
+            Program {
+                code: expected,
+                pc: 8,
+                finished: true
+            },
+            program
+        );
+    }
+
+    #[test]
+    fn execute_instr_with_immediate_mode() {
+        let mut program = Program::new(vec![11100, 10, 20, 0]);
+
+        program.execute_instr(
+            Instr(
+                Opcode::Add(ParameterMode::Immediate, ParameterMode::Immediate),
+                vec![10, 20, 0],
+            ),
+            &mut Cursor::new(""),
+            &mut Cursor::new(Vec::new()),
+        );
+
+        assert_eq!(
+            Program {
+                code: vec![30, 10, 20, 0],
+                pc: 4,
+                finished: false
+            },
+            program
+        );
+    }
+}
+
+#[cfg(test)]
+mod opcode_tests {
+
+    use super::*;
+
+    #[test]
+    fn test_decode_opcode() {
+        assert_eq!(
+            Some(Opcode::Add(
+                ParameterMode::Position,
+                ParameterMode::Position,
+            )),
+            Opcode::decode(1)
+        );
+        assert_eq!(
+            Some(Opcode::Mul(
+                ParameterMode::Position,
+                ParameterMode::Position,
+            )),
+            Opcode::decode(2)
+        );
+        assert_eq!(Some(Opcode::In), Opcode::decode(3));
+        assert_eq!(
+            Some(Opcode::Out(ParameterMode::Position)),
+            Opcode::decode(4)
+        );
+        assert_eq!(Some(Opcode::Fin), Opcode::decode(99));
+        assert_eq!(None, Opcode::decode(5));
+    }
+
+    #[test]
+    fn test_decode_opcode_with_parameters() {
+        assert_eq!(
+            Some(Opcode::Add(
+                ParameterMode::Position,
+                ParameterMode::Immediate,
+            )),
+            Opcode::decode(11001)
+        );
+    }
+}
+
+#[cfg(test)]
+mod instr_tests {
+
+    use super::*;
+
+    #[test]
+    fn test_decode() {
+        assert_eq!(
+            Some(Instr(
+                Opcode::Add(ParameterMode::Position, ParameterMode::Position,),
+                vec![2, 3, 4]
+            )),
+            Instr::decode(&[1, 2, 3, 4])
+        );
+        assert_eq!(
+            Some(Instr(
+                Opcode::Mul(ParameterMode::Position, ParameterMode::Position),
+                vec![3, 4, 5]
+            )),
+            Instr::decode(&[2, 3, 4, 5])
+        );
+        assert_eq!(Some(Instr(Opcode::Fin, vec![])), Instr::decode(&[99]));
+        assert_eq!(Some(Instr(Opcode::In, vec![1])), Instr::decode(&[3, 1]));
+        assert_eq!(
+            Some(Instr(Opcode::Out(ParameterMode::Position), vec![2])),
+            Instr::decode(&[4, 2])
+        );
+    }
+}
+
+#[cfg(test)]
+mod parameter_mode_tests {
+
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(Some(ParameterMode::Position), ParameterMode::parse(0));
+        assert_eq!(Some(ParameterMode::Immediate), ParameterMode::parse(1));
+        assert_eq!(None, ParameterMode::parse(2));
+    }
+}
